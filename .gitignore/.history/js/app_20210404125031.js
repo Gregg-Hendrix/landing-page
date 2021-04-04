ab180def953cf3a184ee1e7ea1dcec220bfd8a61
Gregg Hendrix <gregg@gregghendrix.com>
2021-04-04 13:25:55 -0700

    add .gitignore folder

---
+++ .gitignore/.history/js/app_20210404125031.js
@@ -0,0 +1,59 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+let scroll1 = document.getElementById(section1)
+let scroll2 = document.getElementById(section2)
+let scroll3 = document.getElementById(section3)
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
