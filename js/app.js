66173a398b4409ec31c4f2fd8279192b907d625b
Gregg Hendrix <gregg@gregghendrix.com>
2021-04-15 14:31:29 -0700

    add function to highlight active section

--- js/app.js
+++ js/app.js
@@ -56,21 +56,18 @@ let sectionElements = document.querySelectorAll("section");
 // Convert NodeList to real array - https://developer.mozilla.org/en-US/docs/Web/API/NodeList
 let sections = Array.from(sectionElements);
 for (const section of sections){
-  console.dir(section);
-// TODO: console.log out the dataset.nav property
-
-//  Get nav__list
-let list = document.getElementById("navbar__list");
-//  Create Anchor Link + attach the Href to section
-let linkItem = document.createElement("a");
-linkItem.setAttribute("href", "#" + section.id);
-linkItem.textContent = section.dataset.nav
-//  Create list item, generic name for loop
-let listItem = document.createElement("li");
-//  Appending the Anchor link + Href, to the list item
-list.appendChild(listItem);
-listItem.appendChild(linkItem);
-//  Get correct information from looped item to fill in the Text-Content
+  
+  //  Get nav__list
+  let list = document.getElementById("navbar__list");
+  //  Create Anchor Link + attach the Href to section
+  let linkItem = document.createElement("a");
+  linkItem.setAttribute("href", "#" + section.id);
+  linkItem.textContent = section.dataset.nav
+  //  Create list item, generic name for loop
+  let listItem = document.createElement("li");
+  //  Appending the Anchor link + Href, to the list item
+  list.appendChild(listItem);
+  listItem.appendChild(linkItem);
 }
 
 
@@ -84,9 +81,48 @@ listItem.appendChild(linkItem);
 
 
 // Add class 'active' to section when near top of viewport
+// let linkSelector = document.querySelectorAll(".active");
+// function activeViewport (){
+// let sections = document.querySelectorAll("section");
+let indexLength = sectionElements.length;
+
+function activeViewport(){
+  for(let section of sections){ 
+    const item = section.getBoundingClientRect();
+    const activeElement = document.getElementById(section.id);
+    if (item.y <= 90 && item.bottom >=150){
+      section.classList.add("active");
+      console.log("inside if statement");
+    }
+    else {
+      section.classList.remove("active");
+  }
+    console.log("inside for loop");
+  }
+}
+
+document.addEventListener('scroll', activeViewport);
+  // document.addEventListener('scroll', makeActive);
+
+//   while(-- indexLength && window.scrollY + 50 < sectionElements[indexLength].offsetTop){
+
+//   }
+//   linkSelector.forEach( (link) => link.classList.remove("active"));
+//   linkSelector[indexLength].classList.add("active");
+// }
+// activeViewport();
+// window.addEventListener("scroll", activeViewport);
+
+// If sectionElements = (is in) visualViewport
+// add class active
+
+// else 
+// remove class active
+
 
 
 // Scroll to anchor ID using scrollTO event
+// I'm using css for this, because its better
 
 
 /**
@@ -94,7 +130,16 @@ listItem.appendChild(linkItem);
  * Begin Events
  * 
 */
+window.addEventListener("scroll", function(){
 
+  // get the class of all of the sections
+
+  // remove class from non-active sections (not in viewport)
+
+  // Set active class to section in viewport
+
+  // create a function to loop through sections, to find active class.
+});
 // Build menu 
 
 // Scroll to section on link click
